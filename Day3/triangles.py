5e8c55efa7d597533295e86a184f3960ae0041c3
Jason Buckner <jason@jasonbuckner.com>
2016-12-05 18:13:00 -0800

    Day 3: part 2

--- Day3/triangles.py
+++ Day3/triangles.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
 INPUTS = [line.rstrip('\n') for line in open('input.txt')]
+ROWS = []
 TRIANGLES = []
 
-for candidate in INPUTS:
-    edges = filter(None, candidate.split(' '))
-    triangle = [int(x) for x in edges]
-    TRIANGLES.append(triangle)
+for line in INPUTS:
+    edges = filter(None, line.split(' '))
+    row = [int(x) for x in edges]
+    ROWS.append(row)
+
+transform_index = 0
+row_count = len(ROWS)
+
+while transform_index < row_count:
+    for col in range(0, 3):
+        triangle = []
+        for row in range(transform_index, transform_index + 3):
+            triangle.append(ROWS[row][col])
+        TRIANGLES.append(triangle)
+    transform_index = transform_index + 3
 
 triangle_count = 0
 
