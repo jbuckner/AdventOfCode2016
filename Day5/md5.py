788bedb227e3a0569736b25b05b3919aae9aed80
Jason Buckner <jason@jasonbuckner.com>
2016-12-08 19:42:17 -0800

    Day 5, part 2

--- Day5/md5.py
+++ Day5/md5.py
@@ -3,19 +3,30 @@
 import hashlib
 
 DOORID = 'ojvtpuvg'
+# DOORID = 'abc'
 
 number_found = 0
 counter = 0
-password = ''
+password = list('________')
 
-while number_found < 8:
+def password_complete():
+    for char in password:
+        if char == '_':
+            return False
+    return True
+
+while not password_complete():
     test_string = DOORID + str(counter)
     digest = hashlib.md5(test_string).hexdigest()
     if digest[0:5] == "00000":
-        code = digest[5]
-        print test_string, digest, code
-        password += code
-        number_found += 1
+        char_index = digest[5]
+        if ord(char_index) in range(48, 56):
+            int_index = int(char_index)
+            if password[int_index] == '_':
+                char_to_insert = digest[6]
+                password[int_index] = char_to_insert
+                number_found += 1
+                print password
     counter += 1
 
-print password
+print ''.join(password)
