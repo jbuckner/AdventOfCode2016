c426d6efd12d9da266289940764527839bb6e756
Jason Buckner <jason@jasonbuckner.com>
2016-12-08 23:20:46 -0800

    Day 8, part 1 done!

--- Day8/lcd.py
+++ Day8/lcd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-import sys
 import time
 INPUTS = [line.rstrip('\n') for line in open('input.txt')]
 
 
 class LCD:
+    unlit_pixel = ' '
     width = 0
     height = 0
     matrix = []
@@ -15,7 +15,7 @@ class LCD:
         self.height = height
 
         for y in range(0, self.height):
-            self.matrix.append(['.' for x in range(0, self.width)])
+            self.matrix.append([self.unlit_pixel for x in range(0, self.width)])
 
     def __str__(self):
         return unicode(self)
@@ -26,6 +26,15 @@ class LCD:
             lcd_string += ''.join(row)
             lcd_string += '\n'
         return lcd_string
+
+    @property
+    def lit_pixel_count(self):
+        count = 0
+        for row in self.matrix:
+            for col in row:
+                if col != self.unlit_pixel:
+                    count += 1
+        return count
 
     def draw_rect(self, width, height):
         for y in range(0, height):
@@ -40,8 +49,6 @@ class LCD:
             split = abs(count)
         else:
             split = self.height - abs(count)
-
-        print 'split', split
 
         for y in range(0, self.height):
             char = self.matrix[y][col_x]
@@ -71,23 +78,50 @@ class LCD:
         row[0:count] = left
         row[count:self.width] = right
 
-class Parser:
-    pass
-
-lcd = LCD(50, 10)
-lcd.draw_rect(10, 5)
-
+lcd = LCD(50, 6)
 print lcd
 
-for x in range(0, 25):
-    lcd.rotate_row(0, 1)
+for line in INPUTS:
+    parsed_line = line.split(' ')
+    command = parsed_line[0]
+
+    if command == 'rect':
+        width, height = parsed_line[1].split('x')
+        lcd.draw_rect(int(width), int(height))
+    if command == 'rotate':
+        coordinate = int(parsed_line[2].split('=')[1])  # x=1, it returns the 1
+        distance = int(parsed_line[4])
+        direction = parsed_line[1]
+
+        if direction == 'row':
+            lcd.rotate_row(coordinate, distance)
+        if direction == 'column':
+            lcd.rotate_column(coordinate, distance)
+
     print lcd
     time.sleep(.1)
 
-for x in range(0, 25):
-    lcd.rotate_column(0, -1)
-    print lcd
-    time.sleep(.1)
+print lcd.lit_pixel_count
+# example
+# lcd = LCD(7, 3)
+# lcd.draw_rect(3, 2)
+# print lcd
+# lcd.rotate_column(1, 1)
+# print lcd
+# lcd.rotate_row(0, 4)
+# print lcd
+# lcd.rotate_column(1, 1)
+# print lcd
+
+# for x in range(0, 25):
+#     lcd.rotate_row(0, 1)
+#     print lcd
+#     time.sleep(.1)
+#
+# for x in range(0, 25):
+#     lcd.rotate_column(0, -1)
+#     print lcd
+#     time.sleep(.1)
 
 # lcd.rotate_column(0, 3)
 #
